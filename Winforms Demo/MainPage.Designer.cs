d5cbd97d3ab289eb8e535302bf799bf5c8e18b83
rynak-iqbal <115375081+rynak-iqbal@users.noreply.github.com>
2023-11-22 13:18:01 -0500

    Add project files.

---
+++ Winforms Demo/MainPage.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Winforms_Demo
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.phrases_button = new System.Windows.Forms.Button();
+            this.sounds_button = new System.Windows.Forms.Button();
+            this.music_button = new System.Windows.Forms.Button();
+            this.audiobooks_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // phrases_button
+            // 
+            this.phrases_button.Location = new System.Drawing.Point(0, 0);
+            this.phrases_button.Name = "phrases_button";
+            this.phrases_button.Size = new System.Drawing.Size(464, 70);
+            this.phrases_button.TabIndex = 0;
+            this.phrases_button.Text = "Phrases";
+            this.phrases_button.UseVisualStyleBackColor = true;
+            this.phrases_button.Enter += new System.EventHandler(this.hoverSpeak);
+            // 
+            // sounds_button
+            // 
+            this.sounds_button.Location = new System.Drawing.Point(0, 70);
+            this.sounds_button.Name = "sounds_button";
+            this.sounds_button.Size = new System.Drawing.Size(464, 70);
+            this.sounds_button.TabIndex = 1;
+            this.sounds_button.Text = "Sounds";
+            this.sounds_button.UseVisualStyleBackColor = true;
+            this.sounds_button.Enter += new System.EventHandler(this.hoverSpeak);
+            // 
+            // music_button
+            // 
+            this.music_button.Location = new System.Drawing.Point(0, 140);
+            this.music_button.Name = "music_button";
+            this.music_button.Size = new System.Drawing.Size(464, 70);
+            this.music_button.TabIndex = 2;
+            this.music_button.Text = "Music";
+            this.music_button.UseVisualStyleBackColor = true;
+            this.music_button.Enter += new System.EventHandler(this.hoverSpeak);
+            // 
+            // audiobooks_button
+            // 
+            this.audiobooks_button.Location = new System.Drawing.Point(0, 210);
+            this.audiobooks_button.Name = "audiobooks_button";
+            this.audiobooks_button.Size = new System.Drawing.Size(464, 70);
+            this.audiobooks_button.TabIndex = 3;
+            this.audiobooks_button.Text = "Audiobooks";
+            this.audiobooks_button.UseVisualStyleBackColor = true;
+            this.audiobooks_button.Enter += new System.EventHandler(this.hoverSpeak);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(464, 281);
+            this.Controls.Add(this.audiobooks_button);
+            this.Controls.Add(this.music_button);
+            this.Controls.Add(this.sounds_button);
+            this.Controls.Add(this.phrases_button);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "MainPage";
+            this.Text = "Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button phrases_button;
+        private System.Windows.Forms.Button sounds_button;
+        private System.Windows.Forms.Button music_button;
+        private System.Windows.Forms.Button audiobooks_button;
+    }
+}
+
---
+++ Winforms Demo/MainPage.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Winforms_Demo
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Left:
+                    return true;
+                case Keys.Right:
+                    Console.WriteLine("Select");
+                    Control focusedControl = this.ActiveControl;
+                    (focusedControl as Button).PerformClick();
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+
+        private void hoverSpeak(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            //speech.Volume = 70;
+            //speech.SpeakAsync(s);
+
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "espeak"; // The command to run eSpeak
+            startInfo.Arguments = $"\"{s}\""; // The text to be spoken
+            startInfo.UseShellExecute = false; // Do not use the shell execute process
+            startInfo.CreateNoWindow = true; // Do not create a window
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit(); // Wait for the process to finish
+            }
+
+
+
+
+        }
+        private void hoverLeave(object sender, EventArgs e)
+        {
+            //speech.SpeakAsyncCancelAll();
+        }
+
+        //private void phrases_Click(object sender, EventArgs e)
+        //{
+
+        //    phrases2.Visible = true;
+        //    phrases2.Select();
+        //    Control firstControlWithTabIndex = FindControlByTabIndex(phrases2, 4);
+        //    if (firstControlWithTabIndex != null)
+        //    {
+        //        firstControlWithTabIndex.Focus();
+        //    }
+        //}
+
+        private Control FindControlByTabIndex(Control container, int tabIndex)
+        {
+            foreach (Control control in container.Controls)
+            {
+                if (control.TabIndex == tabIndex)
+                {
+                    return control;
+                }
+            }
+            return null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //phrases2.Visible = false;
+            //music1.Visible = false;
+            //sounds1.Visible = false;
+            //audioBooks1.Visible = false;
+        }
+
+        //public static void OpenPhrasesSub(string category, Form form)
+        //{
+        //    Console.WriteLine(form);
+        //    PhrasesSubMenu phrasesSub = new PhrasesSubMenu(category)
+        //    {
+        //        Location = new System.Drawing.Point(0, 0)
+        //    };
+
+        //    try
+        //    {
+        //        form.Controls.Add(phrasesSub);
+        //        Console.WriteLine(category);
+        //        phrasesSub.Visible = true;
+        //        phrasesSub.BringToFront();
+        //        phrasesSub.Select();
+        //        foreach (Control control in phrasesSub.Controls)
+        //        {
+        //            if (control.TabIndex == 20)
+        //            {
+        //                control.Focus();
+        //            }
+        //        }
+        //        Console.WriteLine("Phrases Sub reached");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+
+        //}
+
+        private void phrases1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //private void musicButton_click(object sender, EventArgs e)
+        //{
+        //    music1.Visible = true;
+        //    music1.BringToFront();
+
+        //    music1.Select();
+        //    Control firstControlWithTabIndex = FindControlByTabIndex(music1, 1);
+        //    if (firstControlWithTabIndex != null)
+        //    {
+        //        firstControlWithTabIndex.Focus();
+        //    }
+
+        //    phrases2.Visible = false;
+        //    sounds1.Visible = false;
+        //    audioBooks1.Visible = false;
+        //}
+        private void musicPage_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void audioBooks_Click(object sender, EventArgs e)
+        //{
+        //    audioBooks1.Visible = true;
+        //    audioBooks1.BringToFront();
+
+        //    audioBooks1.Select();
+        //    Control firstControlWithTabIndex = FindControlByTabIndex(audioBooks1, 1);
+        //    if (firstControlWithTabIndex != null)
+        //    {
+        //        firstControlWithTabIndex.Focus();
+        //    }
+
+        //    phrases2.Visible = false;
+        //    music1.Visible = false;
+        //    sounds1.Visible = false;
+        //}
+
+        private void sounds1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void soundsButton_Click(object sender, EventArgs e)
+        //{
+        //    sounds1.Visible = true;
+        //    sounds1.BringToFront();
+
+        //    sounds1.Select();
+        //    Control firstControlWithTabIndex = FindControlByTabIndex(sounds1, 1);
+        //    if (firstControlWithTabIndex != null)
+        //    {
+        //        firstControlWithTabIndex.Focus();
+        //    }
+
+        //    phrases2.Visible = false;
+        //    music1.Visible = false;
+        //    audioBooks1.Visible = false;
+        //}
+
+        private void audioBooks1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
